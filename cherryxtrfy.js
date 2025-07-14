94ecf1b9c16ed40615aced720f6ab984b396939f
ObiNicklasson <156425800+ObiNicklasson@users.noreply.github.com>
2025-07-14 19:26:26 +0300

    Update cherryxtrfy.js

--- cherryxtrfy.js
+++ cherryxtrfy.js
@@ -1,13 +1,13 @@
-(function(url) {
-'use strict';
+// cherryxtrfy.js
 
-const regex = /-\d+x\d+(?=\.\w{3,4}$)/;
+(function() {
+    'use strict';
+    if (location.hostname.includes("cherryxtrfy.com") && location.pathname.startsWith("/wp/wp-content/uploads/")) {
+        // Регекс шукає "-числоxчисло" перед розширенням
+        const regex = /-\d+x\d+(?=\.\w{3,4}$)/;
 
-// Перевіряємо, чи URL з правильного сайту та чи потрібно видаляти розмірний суфікс
-if (!url.startsWith("https://cherryxtrfy.com/wp/wp-content/uploads/") || !regex.test(url)) {
-return url;
-}
-
-// Видаляємо розмірний суфікс, якщо він є
-return url.replace(regex, "");
-})("вставте_ваш_URL_тут");
+        if (regex.test(location.href)) {
+            location.href = location.href.replace(regex, "");
+        }
+    }
+})();
