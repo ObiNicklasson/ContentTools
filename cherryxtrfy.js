7d67ee1eb175bd6ff2fdd37f3df8cbf996710b83
ObiNicklasson <156425800+ObiNicklasson@users.noreply.github.com>
2025-07-14 19:31:14 +0300

    Update cherryxtrfy.js

--- cherryxtrfy.js
+++ cherryxtrfy.js
@@ -1,13 +1,11 @@
-// cherryxtrfy.js
+'use strict';
 
-(function() {
-    'use strict';
-    if (location.hostname.includes("cherryxtrfy.com") && location.pathname.startsWith("/wp/wp-content/uploads/")) {
-        // Регекс шукає "-числоxчисло" перед розширенням
-        const regex = /-\d+x\d+(?=\.\w{3,4}$)/;
+if (location.hostname.includes("cherryxtrfy.com") && location.pathname.startsWith("/wp/wp-content/uploads/")) {
+    // Регекс шукає "-числоxчисло" перед розширенням
+    const regex = /-\d+x\d+(?=\.\w{3,4}$)/;
 
-        if (regex.test(location.href)) {
-            location.href = location.href.replace(regex, "");
-        }
+    if (regex.test(location.href)) {
+        const newUrl = location.href.replace(regex, "");
+        location.href = newUrl;
     }
-})();
+}
