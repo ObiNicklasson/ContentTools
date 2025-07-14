d66952a68c35ed91e7065becf9fb18dc376c42e6
ObiNicklasson <156425800+ObiNicklasson@users.noreply.github.com>
2025-07-14 19:38:38 +0300

    Update image-helper.user.js

--- image-helper.user.js
+++ image-helper.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Іmage-Рelper (Main)
+// @name         Іmage-Helper (Main)
 // @namespace    http://tampermonkey.net/
 // @version      1.1
 // @description  Головний скрипт
