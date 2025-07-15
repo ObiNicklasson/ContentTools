6451612c16d046813d99d1d3490bf4e29a3bf0ea
ObiNicklasson <156425800+ObiNicklasson@users.noreply.github.com>
2025-07-15 12:27:38 +0300

    Update image-helper.user.js

--- image-helper.user.js
+++ image-helper.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Іmage-Helper
 // @namespace    http://tampermonkey.net/
-// @version      1.0.3
-// @description  Скрипти для відкриття фото у високій якості
+// @version      1.1
+// @description  Скрипт для відкриття фото у високій якості
 // @author       Микола Б.
 
 // @match        https://*.lagguitars.com/*small*
@@ -19,49 +19,35 @@
     'use strict';
 
     // ===== lagguitars =====
-    function handleLagguitars() {
-        if (
-            location.hostname.includes("lagguitars.com") &&
-            location.href.includes("small")
-        ) {
-            location.href = location.href.replace("small", "large");
-        }
+    if (
+        location.hostname.includes("lagguitars.com") &&
+        location.href.includes("small")
+    ) {
+        location.href = location.href.replace("small", "large");
     }
 
     // ===== cherryxtrfy =====
-    function handleCherryxtrfy() {
-        if (location.hostname.includes("cherryxtrfy.com")) {
-            const regex = /-\d+x\d+(?=\.\w{3,4}$)/;
-            if (regex.test(location.href)) {
-                location.href = location.href.replace(regex, "");
-            }
+    if (location.hostname.includes("cherryxtrfy.com")) {
+        const regex = /-\d+x\d+(?=\.\w{3,4}$)/;
+        if (regex.test(location.href)) {
+            location.href = location.href.replace(regex, "");
         }
     }
 
     // ===== Rozetka =====
-    function handleRozetka() {
-        if (
-            location.hostname.includes("rozetka.com.ua") &&
-            location.href.includes("/goods/images/big/")
-        ) {
-            location.href = location.href.replace("big", "original");
-        }
+    if (
+        location.hostname.includes("rozetka.com.ua") &&
+        location.href.includes("/goods/images/big/")
+    ) {
+        location.href = location.href.replace("big", "original");
     }
 
     // ===== lorgar =====
-    function handleLorgar() {
-        if (
-            location.hostname === "cdn0.it4profit.com" &&
-            /h:\d+\/rt:fill\/w:\d+\//.test(location.href)
-        ) {
-            location.href = location.href.replace(/h:\d+\/rt:fill\/w:\d+\//, "");
-        }
+    if (
+        location.hostname === "cdn0.it4profit.com" &&
+        /h:\d+\/rt:fill\/w:\d+\//.test(location.href)
+    ) {
+        location.href = location.href.replace(/h:\d+\/rt:fill\/w:\d+\//, "");
     }
-
-    // ==== Виклики функцій ====
-    handleLagguitars();
-    handleCherryxtrfy();
-    handleRozetka();
-    handleLorgar();
 
 })();
